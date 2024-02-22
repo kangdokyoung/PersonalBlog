0fbc81a0945c97406434df6cdade4a66cce72f1c
DESKTOP-UIGVLB2\ehrud <ehrud6672@naver.com>
2024-02-22 21:23:59 +0900

    Update : 알림창 제작중

--- src/component/alert/AlertBox.tsx
+++ src/component/alert/AlertBox.tsx
@@ -1,19 +1,37 @@
 import styled from "styled-components";
 
 const AlertBox = () => {
-  return <AlertContainer>adsf</AlertContainer>;
+  const alertText = "화면을 나가면 작성된 내용은 사라집니다. 그래도 떠나시겠습니까?";
+  return (
+    <AlertBackground>
+      <AlertContainer>{alertText}</AlertContainer>
+    </AlertBackground>
+  );
 };
 
-const AlertContainer = styled.div`
-  position: absoulte:
+const AlertBackground = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
   top: 0;
-  left : 50vw;
-  width: 220px;
-  height: 120px;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 900;
+`;
+const AlertContainer = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 40%;
+  width: 20%;
+  height: 15%;
   border: 1px solid black;
-  border-radius: 10%;
+  border-radius: 20px;
   background-color: white;
-  z-index: 1000 
+  z-index: 1000;
+  display: flex;
+
+  align-items: center;
+  padding: 12px;
 `;
 
 export default AlertBox;
